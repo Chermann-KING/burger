44a5bfc04aa196be12bbf5d248c36b4b2c07fac3
Chermann-KING <codes.smiling@gmail.com>
2023-07-13 18:34:32 +0200

    ♻️ F01: LOGIN> Create LoginForm

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function LoginPage() {
+export default function LoginForm() {
   // state
   const [inputValue, setInputValue] = useState("");
   // comportement
