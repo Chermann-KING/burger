f92fa207a42b9805d42729c5b2232ed3d435aad1
Chermann-KING <codes.smiling@gmail.com>
2023-07-15 11:22:13 +0200

    💄 F03: LOGO > Add styling and fonts

--- src/components/reusable-ui/Logo.js
+++ src/components/reusable-ui/Logo.js
@@ -1,8 +1,40 @@
 import { styled } from "styled-components";
 
 export default function Logo() {
-  return <LogoStyled>Logo Crazee Burger</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h1>
+        <span>Crazee</span>
+        <img src="./images/logo-orange.png" alt="picto burger" />
+        <span>Burger</span>
+      </h1>
+    </LogoStyled>
+  );
 }
 const LogoStyled = styled.div`
-  background-color: blue;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+  h1 {
+    line-height: 1.6;
+  }
+  h1 > span {
+    display: inline;
+    text-align: center;
+    color: #ffa01b;
+    font-size: 36px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", Cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin-left: 5px;
+    margin-right: 5px;
+    margin-bottom: -17px;
+  }
 `;
