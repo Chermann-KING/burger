1fa863089b901a1b7ee3506bf7170ac3fc907374
Chermann-KING <codes.smiling@gmail.com>
2023-07-16 19:08:58 +0200

    🚧 F04: ORDER > Layout > Add nav and main in container

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import { styled } from "styled-components";
 
 export default function OrderPage() {
   // state
@@ -8,11 +9,44 @@ export default function OrderPage() {
 
   // affiche
   return (
-    <div>
-      <h1>Bonjour {username} !</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="container">
+        <nav className="navbar">
+          Navbar
+          <h1>Bonjour {username} !</h1>
+          <Link to="/">
+            <button>Déconnexion</button>
+          </Link>
+        </nav>
+        <main>Main</main>
+      </div>
+    </OrderPageStyled>
   );
 }
+
+const OrderPageStyled = styled.div`
+  background: orange;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .container {
+    background: red;
+    height: 95vh;
+    width: 1400px;
+    display: flex;
+    flex-direction: column;
+
+    nav {
+      background: blue;
+      height: 10vh;
+      display: flex;
+      justify-content: space-between;
+    }
+    main {
+      background: green;
+      flex: 1;
+    }
+  }
+`;
