87b0a71a7b7ef84b8d2a8343f9d345b8480b3ed9
Chermann-KING <codes.smiling@gmail.com>
2023-07-17 17:45:41 +0200

    🚧 F05: ORDER > Main > Add grid layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -2,14 +2,29 @@ import { styled } from "styled-components";
 import { theme } from "../../../theme/theme";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.main`
   background: ${theme.colors.background_white};
   flex: 1;
 
+  display: grid;
+  grid-template-columns: 25% 1fr;
+
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  .basket {
+    border: 5px solid red;
+  }
+  .menu {
+    border: 5px solid blue;
+  }
 `;
