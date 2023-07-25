818ffab6697ecdee0b65e9d6da1902a91d947eed
Chermann-KING <codes.smiling@gmail.com>
2023-07-25 16:15:14 +0200

    ♻️ F06: Navbar > RightSide > Create <ToastAdmin />

--- src/components/pages/order/Navbar/RightSide.js
+++ src/components/pages/order/Navbar/RightSide.js
@@ -2,9 +2,10 @@ import { styled } from "styled-components";
 import { theme } from "../../../../theme/theme";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
+import ToastAdmin from "./ToastAdmin";
 
 export default function RightSide({ username }) {
   // state
@@ -36,7 +37,7 @@ export default function RightSide({ username }) {
         onToggle={displayToastNofication}
       />
       <Profile username={username} />
-      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <ToastAdmin />
     </RightSideStyled>
   );
 }
@@ -45,22 +46,4 @@ const RightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: ${theme.gridUnit * 6.25}px;
-
-  .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
 `;
