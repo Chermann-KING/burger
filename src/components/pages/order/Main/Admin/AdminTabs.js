d912749957bd9bada579cd06e9424e1f2a234cc7
Chermann-KING <codes.smiling@gmail.com>
2023-07-27 14:58:11 +0200

    ♻️ F07: ORDER > Main > Admin > Replace selectAddTab() and selectEditTab() with selectTab()

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -19,15 +19,18 @@ export default function AdminTabs() {
   } = useContext(OrderContext);
 
   // comportement
-  const selectAddTab = () => {
+  const selectTab = (tabSelected) => {
     setIsCollapsed(false);
-    setIsAddSelected(true);
-    setIsEditSelected(false);
-  };
-  const selectEditTab = () => {
-    setIsCollapsed(false);
-    setIsEditSelected(true);
-    setIsAddSelected(false);
+
+    if (tabSelected === "add") {
+      setIsAddSelected(true);
+      setIsEditSelected(false);
+    }
+
+    if (tabSelected === "edit") {
+      setIsEditSelected(true);
+      setIsAddSelected(false);
+    }
   };
 
   // Affichage
@@ -42,13 +45,13 @@ export default function AdminTabs() {
       <Tab
         label="Ajouter un produit"
         Icon={<AiOutlinePlus />}
-        onClick={selectAddTab}
+        onClick={() => selectTab("add")}
         className={isAddSelected ? "is-active" : ""}
       />
       <Tab
         label="Modifier un produit"
         Icon={<MdModeEditOutline />}
-        onClick={selectEditTab}
+        onClick={() => selectTab("edit")}
         className={isEditSelected ? "is-active" : ""}
       />
     </AdminTabsStyled>
